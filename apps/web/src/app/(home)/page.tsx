be0ed304952dd11c8fb2f168d0b1c5db756568ff
Gusales <dev.gussales@gmail.com>
2025-02-18 16:39:14 -0300

    Feat(web): Ajustes de responsividade para desktops

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -8,30 +8,32 @@ import Image from 'next/image';
 export default function Home() {
   return (
    <div>
-    <header className="w-full flex flex-col items-center gap-5">
-      <Image src={logo} alt="DevStage Logo" className="w-[86.8px] h-6"/>
+    <header className="w-full flex flex-col items-center md:items-start gap-5">
+      <Image src={logo} alt="DevStage Logo" className="w-[86.8px] h-6 md:w-[108.5px] md:h-[30px]"/>
 
-      <div className="text-4xl leading-tight font-semibold text-center">
+      <div className="text-4xl md:text-7xl leading-tight font-semibold text-center md:text-left">
         <h1 className="text-blue">CodeCraft</h1>
         <h2>Summit 2025</h2>
       </div>
 
     </header>
 
-    <main className="mt-10 space-y-4">
+    <main className="mt-10 space-y-4 md:space-y-0 md:flex md:gap-5">
       <section className="bg-gray-700 rounded-2xl p-6 flex flex-col gap-6 border border-gray-600">
         <header className="w-full flex justify-between items-center">
-          <h3 className="text-gray-200 text-xl leading-none font-semibold">Sobre o evento</h3>
+          <h3 className="text-gray-200 text-xl md:text-2xl leading-none font-semibold">Sobre o evento</h3>
           <p className="flex items-center gap-2 text-purple uppercase font-semibold leading-relaxed"><Radio /> ao vivo</p>
         </header>
 
-        <p className="text-gray-300 text-sm leading-relaxed">
+        <p className="text-gray-300 text-sm md:text-base leading-relaxed">
         Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons. <br />
+        <br /><br />
+        
         Dias 15 a 17 de março | Das 18h às 21h
         </p>
       </section>
 
-      <form action="" className="bg-gray-700 rounded-2xl p-6 flex flex-col gap-6 border border-gray-600">
+      <form action="" className="bg-gray-700 rounded-2xl p-6 flex flex-col gap-6 border border-gray-600 md:min-w-[441px]">
         <h3 className="text-gray-200 text-xl leading-none font-semibold">Incrição</h3>
 
         <InputContainer>
